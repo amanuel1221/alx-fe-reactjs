ebbbc7dcfbef2702d275126f11ba557010a3a28b
Amanuel <beginningoflife2324@gmail.com>
2025-08-28 05:10:46 -0700

    create user profiles

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function Userprofile(props){
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
+        </div>
+    )
+}
+export default Userprofile;
