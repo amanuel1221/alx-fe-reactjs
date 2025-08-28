0e22a5de2724aa80c42e8712cb9757afc1d0cd41
Amanuel <beginningoflife2324@gmail.com>
2025-08-28 05:14:31 -0700

    improve the structures of components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,9 +7,9 @@ import Header from './components/Header.jsx';
 
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
-import Userprofile from './components/UserProfile.jsx'; 
+import UserProfile from './components/UserProfile.jsx'; 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
