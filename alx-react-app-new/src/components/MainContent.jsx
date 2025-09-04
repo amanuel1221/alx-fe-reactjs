6886839b4b4da06bfacb55b747092e3b97066138
Amanuel <beginningoflife2324@gmail.com>
2025-09-04 04:55:25 -0700

    styling the contents with inline style

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p style={{ color: 'darkgreen', fontStyle: 'italic' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
