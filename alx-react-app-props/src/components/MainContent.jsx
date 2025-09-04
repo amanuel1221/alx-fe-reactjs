f9e1ea210a8e4918e06125c248ca98c17bad760b
Amanuel <beginningoflife2324@gmail.com>
2025-09-04 05:38:39 -0700

    working with usercontext

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: 'darkgreen', fontStyle: 'italic' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
