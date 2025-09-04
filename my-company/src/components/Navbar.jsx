1595157b56ebebe04df2198d940d6baf33a3dc57
Amanuel <beginningoflife2324@gmail.com>
2025-09-04 06:46:35 -0700

    style the bar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
-      <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px" }}>Contact</Link>
+    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px", backgroundColor: "#eee" }}>
+      <div>
+        <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
+        <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
+        <Link to="/services" style={{ margin: "0 10px" }}>Services</Link>
+        <Link to="/contact" style={{ margin: "0 10px" }}>Contact</Link>
+      </div>
     </nav>
   );
 }
