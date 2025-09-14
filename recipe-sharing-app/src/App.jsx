7a5cdee25bf360d6a6f2fdda21794ef2609ba72e
Amanuel <beginningoflife2324@gmail.com>
2025-09-14 11:17:28 -0700

    structured all folders

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,11 @@
+
 import { useState } from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
-import useRecipeStore from "./store/recipeStore";
+import { useRecipeStore } from "./components/recipeStore";
+
+import { create } from "zustand";
+
 import "./App.css";
 
 function App() {
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
   // RecipeList component
-import useRecipeStore from '../store/recipeStore';
+import {useRecipeStore} from './recipeStore'
+
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
