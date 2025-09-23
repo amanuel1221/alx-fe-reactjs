691e98f7d98d7092e85fc4bbdf2b05e5450ddc35
Amanuel <beginningoflife2324@gmail.com>
2025-09-23 04:56:14 -0700

    woking with interactivity

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users/";
+const BASE_URL = "https://api.github.com";
 
-async function fetchUserData(username) {
-  const response = await axios.get(`${BASE_URL}${username}`);
+// Advanced search using GitHub’s search API
+export async function fetchAdvancedUserData(username, location, minRepos) {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users?q=${query.trim()}`);
   return response.data;
 }
-
-export default fetchUserData;
