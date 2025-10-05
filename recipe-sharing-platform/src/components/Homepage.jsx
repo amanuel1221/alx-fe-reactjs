575d8b2ff33fb3e64bb0cf981fd26df62065a3f1
Amanuel <beginningoflife2324@gmail.com>
2025-10-05 01:02:02 -0700

    update sections of homepage

--- recipe-sharing-platform/src/components/Homepage.jsx
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";   
-import data from "../store/data.json";
+import data from "../data.json";
 const Homepage = () => {
     const [recipes ,setRecipes] = useState([]);
     useEffect(()=>{
